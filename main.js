26ada446386518b1ca2cedbfc651476d861dc0ad
IamCosima <uzairsarvan@gmail.com>
2025-04-06 11:21:08 +0200

    Fixed gethumanchoice and finished playround function

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-console.log("Hello World")
+humanScore = 0;
+computerScore = 0;
+
 
 function getComputerChoice() {
     seed = Math.random();
@@ -13,11 +15,40 @@ function getComputerChoice() {
     }
      return move  
 }
-console.log(getComputerChoice())
 
 function getHumanChoice() {
-    choice = prompt("rock, paper, scissors. Shoot!")
-    return choice
-    
+    return prompt("rock, paper, scissors. Shoot!")   
 }
-console.log(getHumanChoice())
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    //rock beats scissors looses to paper
+    //scissors looses to rock and beats paper
+    //papper beats rock but looses to scissors
+    
+    if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        return console.log("You Win! Rock beats Scissors");
+    }
+    else if (humanChoice === 'scissors' && computerChoice === 'paper'){
+        return console.log("You Win! Scissors beats Paper");
+
+    }
+    else if (humanChoice === 'paper' && computerChoice === 'rock'){
+        return console.log("You Win! Paper beats Rock");
+    }
+    else if (humanChoice === computerChoice){
+        return console.log("You Tie! " + computerChoice + ' draws ' + humanChoice);
+    }
+
+    else {
+        return console.log("You Loose! " + computerChoice + ' beats ' + humanChoice);
+    }
+  }
+  
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+  console.log("Human: " + humanSelection);
+  console.log("Computer: " + computerSelection);
+  
+  playRound(humanSelection, computerSelection);
+  
